97b3934cf20be22e748c99f84e10bf6e222ce05e
Craig Merchant <56309+CraigMerchant@users.noreply.github.com>
2021-06-15 11:52:33 +0100

    Capture latest screenshot

--- take_screenshot.js
+++ take_screenshot.js
@@ -1,16 +1,24 @@
 const fs = require("fs-extra");
-const request = require('request');
+const request = require("request");
 
 const sampleBase = "https://craigmerchant.dev";
 
-function download(uri, filename, callback){
-  request.head(uri, function(err, res, body){
-    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+function download(uri, filename, callback) {
+  request.head(uri, function(err, res, body) {
+    request(uri)
+      .pipe(fs.createWriteStream(filename))
+      .on("close", callback);
   });
-};
+}
 
 console.log("taking screenshot");
 
-download('https://scrapshotter.herokuapp.com/?vw=1600&vh=850&resize=true&w=1200&h=638&url='+sampleBase+'&output=binary', './site/assets/screenshot.jpg', function(){
-  console.log("Screenshot captured");
-});
+download(
+  "https://scrapshotter.herokuapp.com/?vw=1600&vh=850&resize=true&w=950&h=638&url=" +
+    sampleBase +
+    "&output=binary",
+  "./site/assets/screenshot.jpg",
+  function() {
+    console.log("Screenshot captured");
+  }
+);
